c31d79f48bb078894b7995ebbede0554600db098
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2025-03-27 23:03:48 +0100

    feat(event): get single event

--- db/db.go
+++ db/db.go
@@ -10,27 +10,29 @@ var DB *sql.DB
 func InitDB() {
 
 	var err error
-	DB, err := sql.Open("sqlite3", "api.db")
+	DB, err = sql.Open("sqlite3", "api.db")
 	if err != nil {
 		panic("db could not connect")
 	}
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+
+	createTables()
 }
 
 func createTables() {
 
 	createEventsTable := `
-     CREATE TABLE IF NOT EXISTS events (
-        id INTEGER PRIMARY KEY AUTOINCREMENT
-        name TEXT NOT NULL,
-        description TEXT NOT NULL,
-        location TEXT NOT NULL,
-        dateTime DATETIME NOT NULL,
-        user_id INTEGER                               
-     )
-`
+	    CREATE TABLE IF NOT EXISTS events (
+	       id INTEGER PRIMARY KEY AUTOINCREMENT,
+	       name TEXT NOT NULL,
+	       description TEXT NOT NULL,
+	       location TEXT NOT NULL,
+	       dateTime DATETIME NOT NULL,
+	       user_id INTEGER
+	    )
+	`
 	_, err := DB.Exec(createEventsTable)
 	if err != nil {
 		panic("could not create events table")
--- main.go
+++ main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/inuoluwadunsimi/event-booker/db"
 	"github.com/inuoluwadunsimi/event-booker/models"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.POST("/events", createEvent)
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(ctx *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events"})
+	}
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "yeah?", "data": events})
 
@@ -36,6 +44,27 @@ func createEvent(ctx *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 	event.Save()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not create events"})
+	}
+
 	ctx.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
+
+}
+
+func getEvent(ctx *gin.Context) {
+
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "invalid event Id"})
+
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event"})
+	}
+
+	ctx.JSON(http.StatusOK, event)
 
 }
